8d3a787b602fa52da2e71712e26311b3ac336677
Jameel Alsalam <alsalam.jameel@epa.gov>
2021-08-06 11:39:12 -0400

    example project with my Rmd setup, and tabsets

---
+++ _targets.R
@@ -0,0 +1,28 @@
+
+library(targets)
+library(tarchetypes)
+
+# This is an example _targets.R file. Every
+# {targets} pipeline needs one.
+# Use tar_script() to create _targets.R and tar_edit()
+# to open it again for editing.
+# Then, run tar_make() to run the pipeline
+# and tar_read(summary) to view the results.
+
+# Define custom functions and other global objects.
+# This is where you write source(\"R/functions.R\")
+# if you keep your functions in external scripts.
+
+
+# Set target-specific options such as packages.
+# tar_option_set(packages = "dplyr")
+
+# End this file with a list of target objects.
+list(
+  tar_render(
+    penguin_tabs_report,
+    "docs/penguin-tabs-main.Rmd",
+    output_dir = "output",
+    output_file = "penguin-tabs-targets.html"
+  )
+)
